21148f56b1a4711328f3057801fdb9938e197844
Huy Duong <huy.duongdinh@gmail.com>
2021-08-16 21:59:07 +0700

    Show git blame

--- lua/git/blame.lua
+++ lua/git/blame.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+local function blameLinechars()
+  local chars = vim.fn.strlen(
+    vim.fn.substitute(vim.fn.matchstr(vim.fn.getline ".", [[.\{-\}\s\+\d\+\ze)]]), [[\v\C.]], ".", "g")
+  )
+  if vim.fn.exists "*synconcealed" and vim.wo.conceallevel > 1 then
+    for col = 1, chars do
+      chars = chars - vim.fn.synconcealed(vim.fn.line ".", col)[0]
+    end
+  end
+
+  return chars
+end
+
 local function create_blame_win()
   vim.api.nvim_command "topleft vnew"
   local win = vim.api.nvim_get_current_win()
@@ -11,22 +24,27 @@ local function create_blame_win()
   vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
   vim.api.nvim_buf_set_option(buf, "filetype", "git.nvim")
   vim.api.nvim_buf_set_option(buf, "buflisted", false)
-
   vim.api.nvim_win_set_option(win, "wrap", false)
-  vim.api.nvim_win_set_width(win, 40)
+
+  -- TODO: Rewrite in lua
+  vim.cmd [[setlocal nonumber scrollbind nowrap foldcolumn=0 nofoldenable winfixwidth]]
+  vim.cmd [[setlocal signcolumn=no]]
 end
 
 function M.blame()
-  local fpath = vim.fn.expand('%:p')
+  local fpath = vim.fn.expand "%:p"
   create_blame_win()
-  vim.api.nvim_command('read!git --literal-pathspecs --no-pager -c blame.coloring=none -c blame.blankBoundary=false blame --show-number -- ' .. fpath)
---   vim.fn.jobstart(cmd, {
---     on_stderr = on_event,
---     on_stdout = on_event,
---     on_exit = on_event,
---     stdout_buffered = true,
---     stderr_buffered = true,
---   })
+  -- TODO: Handle error + using job
+  vim.api.nvim_command(
+    "read!git --literal-pathspecs --no-pager -c blame.coloring=none -c blame.blankBoundary=false blame --show-number -- "
+      .. fpath
+  )
+  vim.api.nvim_win_set_width(0, blameLinechars() + 1)
+
+  -- Delete the empty first line
+  -- FIXME: Find better solution to handle the empty line
+  vim.cmd('normal gg')
+  vim.cmd('normal dd')
 end
 
 return M
