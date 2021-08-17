edca4ce8e0e422b9b1c07c6aa1ee925a86465495
Huy Duong <huy.duongdinh@gmail.com>
2021-08-17 18:43:13 +0700

    Using job to run blame command

--- lua/git/blame.lua
+++ lua/git/blame.lua
@@ -34,36 +34,17 @@ local function create_blame_win()
   vim.api.nvim_win_set_option(win, "winfixwidth", true)
   vim.api.nvim_win_set_option(win, "signcolumn", "no")
 
-  return win
+  return win, buf
 end
 
-function M.blame_commit()
-  vim.notify("TBD")
-end
-
-function M.blame()
-  local fpath = vim.fn.expand "%:p"
-  if fpath == "" or fpath == nil then
-    return
-  end
-
+local function on_blame_done(lines)
   local starting_win = vim.api.nvim_get_current_win()
   local current_pos = vim.api.nvim_win_get_cursor(starting_win)
+  local blame_win, blame_buf = create_blame_win()
 
-  local blame_win = create_blame_win()
-
-  -- TODO: Handle error + using job
-  vim.api.nvim_command(
-    "read!git --literal-pathspecs --no-pager -c blame.coloring=none -c blame.blankBoundary=false blame --show-number -- "
-      .. fpath
-  )
-  vim.api.nvim_win_set_width(0, blameLinechars() + 1)
-
-  -- Delete the empty first line
-  -- FIXME: Find better solution to handle the empty line
-  vim.cmd "normal gg"
-  vim.cmd "normal dd"
-
+  vim.api.nvim_buf_set_lines(blame_buf, 0, -1, true, lines)
+  vim.api.nvim_buf_set_option(blame_buf, "modifiable", false)
+  vim.api.nvim_win_set_width(blame_win, blameLinechars() + 1)
   vim.api.nvim_win_set_cursor(blame_win, current_pos)
   -- TODO: Restore these options when blame windown is closed
   vim.api.nvim_win_set_option(starting_win, "scrollbind", true)
@@ -78,6 +59,46 @@ function M.blame()
 
   vim.api.nvim_buf_set_keymap(0, "n", "q", "<CMD>q<CR>", options)
   vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CMD>lua require('git.blame').blame_commit()<CR>", options)
+end
+
+function M.blame_commit()
+  vim.notify "TBD"
+end
+
+function M.blame()
+  local fpath = vim.fn.expand "%:p"
+  if fpath == "" or fpath == nil then
+    return
+  end
+
+  local blame_cmd = "git --literal-pathspecs --no-pager -c blame.coloring=none -c blame.blankBoundary=false blame --show-number -- "
+    .. fpath
+
+  local lines = {}
+
+  local function on_event(_, data, event)
+    if event == "stdout" or event == "stderr" then
+      if data then
+        for i = 1, #data do
+          if data[i] ~= "" then
+            table.insert(lines, data[i])
+          end
+        end
+      end
+    end
+
+    if event == "exit" then
+      on_blame_done(lines)
+    end
+  end
+
+  vim.fn.jobstart(blame_cmd, {
+    on_stderr = on_event,
+    on_stdout = on_event,
+    on_exit = on_event,
+    stdout_buffered = true,
+    stderr_buffered = true,
+  })
 end
 
 return M
