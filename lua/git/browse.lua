e7e2cffc9a9619aac47ac016f1c50f894df36fec
Huy Duong <huy.duongdinh@gmail.com>
2021-08-19 16:31:40 +0700

    Git browse pull request

--- lua/git/browse.lua
+++ lua/git/browse.lua
@@ -34,8 +34,42 @@ local function get_git_remote_url()
   )
 end
 
+local function get_gitlab_merge_request_url(git_remote_url, commit_hash)
+  local merge_request = utils.run_git_cmd(
+    'git ls-remote origin "*/merge-requests/*/head" | grep ' .. commit_hash .. ' | tr -d "\n"'
+  )
+  if merge_request == nil or merge_request == "" then
+    utils.log("Failed to get merge request from commit hash " .. commit_hash)
+    return
+  end
+
+  -- TODO: Handle in lua
+  local merge_request_id = vim.fn.system("echo " .. merge_request .. [[ | awk -F'/' '{print $3}' | tr -d "\n"]])
+
+  return git_remote_url .. "/merge_requests/" .. merge_request_id
+end
+
+local function get_github_pull_request_url(git_remote_url, commit_hash)
+  local pull_request = utils.run_git_cmd(
+    'git ls-remote origin "refs/pull/*/head" | grep ' .. commit_hash .. ' | tr -d "\n"'
+  )
+  if pull_request == nil or pull_request == "" then
+    utils.log("Failed to get pull request from commit hash " .. commit_hash)
+    return
+  end
+
+  -- TODO: Handle in lua
+  local pull_request_id = vim.fn.system("echo " .. pull_request .. [[ | awk -F'/' '{print $3}' | tr -d "\n"]])
+
+  return git_remote_url .. "/pull/" .. pull_request_id
+end
+
 local function get_current_branch_name()
   return utils.run_git_cmd 'git rev-parse --abbrev-ref HEAD | tr -d "\n"'
+end
+
+local function get_lastest_commit_hash(branch_name)
+  return utils.run_git_cmd("git rev-parse " .. "origin/" .. branch_name .. ' | tr -d "\n"')
 end
 
 local function get_git_site_type(git_remote_url)
@@ -89,6 +123,45 @@ function M.open(visual_mode)
     end
   else
     open_url(git_remote_url .. "/tree/" .. branch_name .. "/")
+  end
+end
+
+function M.pull_request()
+  local git_remote_url = get_git_remote_url()
+  if git_remote_url == nil then
+    utils.log "Failed to get git remote url"
+    return
+  end
+
+  local branch_name = get_current_branch_name()
+  if branch_name == nil then
+    utils.log "Failed to get branch name"
+    return
+  end
+
+  local git_site_type = get_git_site_type(git_remote_url)
+  if git_site_type == nil then
+    utils.log "Git site is not supported"
+    return
+  end
+
+  utils.log "Opening pull request..."
+
+  local latest_commit_hash = get_lastest_commit_hash(branch_name)
+  if latest_commit_hash == nil then
+    utils.log("Failed to get the lastest commit from branch: " .. branch_name)
+    return
+  end
+
+  local url = nil
+  if git_site_type == GitType.GITHUB then
+    url = get_github_pull_request_url(git_remote_url, latest_commit_hash)
+  else
+    url = get_gitlab_merge_request_url(git_remote_url, latest_commit_hash)
+  end
+
+  if url ~= nil then
+    open_url(url)
   end
 end
 
