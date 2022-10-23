53b288eca034ad130666b8e55fe84236027fde46
Huy Duong <huy.duongdinh@gmail.com>
2022-10-24 00:06:29 +0700

    Using plenary

--- lua/git/status.lua
+++ lua/git/status.lua
@@ -3,6 +3,7 @@ local utils = require "git.utils"
 local git = require "git.utils.git"
 
 local status_state = {
+  bufnr = -1,
   untracked = {},
   unstaged = {},
   staged = {},
@@ -10,6 +11,36 @@ local status_state = {
 }
 
 local M = {}
+
+local function contains_file(files, file)
+  for _, f in ipairs(files) do
+    if f.file == file then
+      return true
+    end
+  end
+
+  return false
+end
+
+--TODO: Move this method to git utils
+local function git_cmd(args)
+  local git_root = git.get_git_repo()
+  if git_root == "" then
+    return 1, { "" }
+  end
+
+  local stderr = {}
+  local stdout, ret = Job:new({
+    command = "git",
+    args = args,
+    cwd = git_root,
+    on_stderr = function(_, data)
+      table.insert(stderr, data)
+    end,
+  }):sync()
+
+  return ret, stdout, stderr
+end
 
 local function add_blankline()
   table.insert(status_state.output, "")
@@ -39,17 +70,7 @@ local function query_log(refspec, limit)
     return nil
   end
 
-  local lines_string = ""
-  local stderr = {}
-  local stdout, _ = Job:new({
-    command = "git",
-    args = { "log", "-n", limit, "--pretty=format:%h%x09%s", refspec, "--" },
-    cwd = git_root,
-    on_stderr = function(_, data)
-      table.insert(stderr, data)
-    end,
-  }):sync()
-
+  local _, stdout, stderr = git_cmd { "log", "-n", limit, "--pretty=format:%h%x09%s", refspec, "--" }
   if not vim.tbl_isempty(stderr) then
     return {}
   end
@@ -82,14 +103,19 @@ local function clear_state()
   status_state.output = {}
 end
 
-local function render(lines, open_new_buffer)
+local function render(lines)
   while #lines > 0 and string.find(lines[1], "^%l+:") do
     table.remove(lines, 1)
   end
 
-  if open_new_buffer then
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  if bufnr ~= status_state.bufnr then
     vim.api.nvim_command "tabedit"
   end
+
+  local status_buf = vim.api.nvim_get_current_buf()
+  status_state.bufnr = status_buf
 
   local head = vim.fn.matchstr(lines[1], [[^## \zs\S\+\ze\%($\| \[\)]])
   local branch = ""
@@ -107,8 +133,6 @@ local function render(lines, open_new_buffer)
   else
     branch = head
   end
-
-  local status_buf = vim.api.nvim_get_current_buf()
 
   -- clear status state
   clear_state()
@@ -158,91 +182,45 @@ local function render(lines, open_new_buffer)
   vim.api.nvim_buf_set_keymap(0, "n", "r", "<CMD>lua require('git.status').refresh<CR>", options)
 end
 
-local function on_refresh_done(lines)
-  render(lines, false)
-end
-
-local function on_status_done(lines)
-  render(lines, true)
-end
-
-local function get_status_cmd()
-  local git_root = git.get_git_repo()
-  if git_root == "" then
-    return nil
-  end
-
-  return "git -C " .. git_root .. " --no-optional-locks status --porcelain -b"
-end
-
-local function refresh()
-  local status_cmd = get_status_cmd()
-  if status_cmd == nil then
-    return
-  end
-
-  utils.jobstart(status_cmd, on_refresh_done)
-end
-
-local function on_cmd_done(lines)
-  refresh()
-end
-
-local function contains_file(files, file)
-  for _, f in ipairs(files) do
-    if f.file == file then
-      return true
-    end
-  end
-
-  return false
-end
-
 function M.toggle_status()
   local line = vim.api.nvim_get_current_line()
   if line == nil or line == "" then
     return
   end
-  local git_root = git.get_git_repo()
-  if git_root == "" then
-    return
-  end
 
-  local cmd = ""
+  local cmd_args = {}
   if utils.starts_with(line, "Unstaged ") then
-    cmd = "git -C " .. git_root .. " add -u"
+    cmd_args = { "add", "-u" }
   elseif utils.starts_with(line, "Untracked ") then
-    cmd = "git -C " .. git_root .. " add ."
+    cmd_args = { "add", "." }
   elseif utils.starts_with(line, "Staged ") then
-    cmd = "git -C " .. git_root .. " reset -q"
+    cmd_args = { "reset", "-q" }
   elseif utils.contains(line, " ") then
     local file = utils.split(line, " ")[2]
     --FIXME: I know that this is not a good way to find the toggle command for the current line
     -- I can do better, however for sake of simplicity I would like brute force to find the appropriate cmd
     -- The git status files size is not large, I don't think it bring a big impact to the plugin's performance
     if contains_file(status_state.staged, file) then
-      cmd = "git -C " .. git_root .. " reset -q -- " .. file
+      cmd_args = { "reset", "-q", "--", file }
     elseif contains_file(status_state.unstaged, file) then
-      cmd = "git -C " .. git_root .. " add -A -- " .. file
+      cmd_args = { "add", "-A", "--", file }
     elseif contains_file(status_state.untracked, file) then
-      cmd = "git -C " .. git_root .. " add -- " .. file
+      cmd_args = { "add", "--", file }
     end
   end
 
-  if cmd == "" then
+  if vim.tbl_isempty(cmd_args) then
     return
   end
 
-  utils.jobstart(cmd, on_cmd_done)
+  git_cmd(cmd_args)
+  -- reload status
+  M.open()
 end
 
 function M.open()
-  local status_cmd = get_status_cmd()
-  if status_cmd == nil then
-    return
-  end
-
-  utils.jobstart(status_cmd, on_status_done)
+  local _, lines, _ = git_cmd { "--no-optional-locks", "status", "--porcelain", "-b" }
+  render(lines)
 end
 
 return M
