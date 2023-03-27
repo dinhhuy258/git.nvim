19a229de4a6ed4d201b36b596de63fb7f7d55eda
Huy Duong <dinhhuy258@users.noreply.github.com>
2023-03-27 11:31:43 +0700

    Fix git browse build wrong url (#22)

--- lua/git/browse/git_server.lua
+++ lua/git/browse/git_server.lua
@@ -1,6 +1,7 @@
 local git = require "git.utils.git"
 local path = require "git.utils.path"
 local log = require "git.utils.log"
+local utils = require "git.utils"
 
 local GitServer = {}
 
@@ -27,7 +28,13 @@ end
 function GitServer:_open(visual_mode)
   local current_file = vim.fn.expand "%:h"
   if current_file ~= nil and current_file ~= "" then
+    local git_root = git.get_git_repo()
+    local absolute_path = vim.fn.expand "%:p"
     local relative_path = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.")
+    -- TODO: moving starts_with method to utils/string.lua
+    if utils.starts_with(absolute_path, git_root) then
+      relative_path = absolute_path:sub(#git_root + 1)
+    end
 
     if not visual_mode then
       GitServer._open_url(path.join { self.git_url, self.path, "blob", self.branch, relative_path })
--- lua/git/browse/init.lua
+++ lua/git/browse/init.lua
@@ -5,28 +5,30 @@ local GitServerFactory = require "git.browse.git_server_factory"
 local M = {}
 
 function M.open(visual_mode)
+  log.info "Opening Git repository..."
+
   local git_server = GitServerFactory.get_git_server()
   if git_server == nil then
     return
   end
-
-  log.info "Opening Git repository..."
 
   git_server:open(visual_mode)
 end
 
 function M.pull_request()
+  log.info "Opening current pull request..."
+
   local git_server = GitServerFactory.get_git_server()
   if git_server == nil then
     return
   end
 
-  log.info "Opening current pull request..."
-
   git_server:open_pull_request()
 end
 
 function M.create_pull_request(target_branch)
+  log.info "Creating pull request..."
+
   local git_server = GitServerFactory.get_git_server()
   if git_server == nil then
     return
@@ -36,8 +38,6 @@ function M.create_pull_request(target_branch)
   if target_branch ~= nil and target_branch ~= "" then
     git_target_branch = target_branch
   end
-
-  log.info "Creating pull request..."
 
   git_server:create_pull_request(git_target_branch)
 end
