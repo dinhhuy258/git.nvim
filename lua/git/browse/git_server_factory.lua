7a342e61f28e321ef08e00fca4be74ff3ef88335
Selman Kayrancioglu <seruman@users.noreply.github.com>
2023-06-20 14:53:28 +0300

    git/browse: handle ssh URLs (#24)

--- lua/git/browse/git_server_factory.lua
+++ lua/git/browse/git_server_factory.lua
@@ -20,6 +20,9 @@ local function _get_git_remote_url()
   if remote_url:find "^git@" then
     git_server, git_path = remote_url:match "^git@([^:/]+):(.+)"
     git_server = "https://" .. git_server
+  elseif remote_url:find "^ssh://git@" then
+    git_server, git_path = remote_url:match "^ssh://git@([^:/]+)/(.+)"
+    git_server = "https://" .. git_server
   else
     git_server, git_path = remote_url:match "^(https?://[^/]+)/(.+)"
   end
